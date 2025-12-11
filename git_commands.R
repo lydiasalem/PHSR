2e6a9caed73aad8234b87cf04c862831bb17d19b
Lydia Salem <shlemmy1@gmail.com>
2025-12-11 09:51:01 +0000

    added hello

--- git_commands.R
+++ git_commands.R
@@ -3,3 +3,4 @@ library(usethis)
 use_git_config(user.name = "Lydia Salem", user.email = "shlemmy1@gmail.com")
 gitcreds::gitcreds_set()
 git remote add origin https://github.com/LYDIASALEM/PHSR.git
+#hello
